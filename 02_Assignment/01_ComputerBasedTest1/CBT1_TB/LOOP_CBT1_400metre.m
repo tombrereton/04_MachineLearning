c07236fa5ecb18c352cf23487e200215fc321710
tombrereton <tbererton89@gmail.com>
2016-10-15 21:04:00 +0100

    Cross validation running for polynomial of order n

--- 02_Assignment/01_ComputerBasedTest1/CBT1_TB/LOOP_CBT1_400metre.m
+++ 02_Assignment/01_ComputerBasedTest1/CBT1_TB/LOOP_CBT1_400metre.m
@@ -26,16 +26,15 @@ partition = floor(N/totalFolds);
 %%
 % Polynomial Order of n
 % Changing value of 'order' to choose polynomial order
-order = 3;
+order = 8;
 X = [];
 Xtest = [];
 
-% for k = 0:order
-    X = [X x.^0];
-%     Xtest1 = [Xtest x.^k];
+ for k = 0:order
+     X = [X x.^k];
     
     for fold = 0:totalFolds-1
-        Xtest = X((fold*partition)+1 : (fold+1)*partition,:);
+        Xfold = X((fold*partition)+1 : (fold+1)*partition,:);
         Xtrain1 = X(1 : fold*partition,:);
         Xtrain2 = X(fold*partition + (partition + 1) : end,:);
         Xtrain = [Xtrain1; Xtrain2];
@@ -43,56 +42,32 @@ Xtest = [];
         ttrain1 = t(1 : fold*partition,1);
         ttrain2 = t(fold*partition + (partition + 1) : end,1);
         ttrain = [ttrain1; ttrain2];
+        tfold = t((fold*partition)+1 : (fold+1)*partition,:);
 
         % compute model parameters
         w = inv(Xtrain'*Xtrain)*Xtrain'*ttrain; 
         % model predictions
-        mpred_test = w'*Xtest';
+        mpred_fold = w'*Xfold';
         % model for observed data
         mpred_observed = w'*Xtrain';
         % compare values
-        compare = [Xtest mpred_test' Xtest-mpred_test'];
+        compare = [tfold mpred_fold' tfold-mpred_fold'];
         % loss
 
-        foldLoss(fold+1,1)  = mean((Xtest-mpred_test').^2);
-        trainLoss(fold+1,1) = mean((Xtrain-mpred_observed').^2);
+        foldLoss(fold+1,k+1)  = mean((mpred_fold' - tfold).^2);
+        trainLoss(fold+1,k+1) = mean((mpred_observed' - ttrain).^2);
     end
-% end
+ end
 
-% %% plot of loss
-% figure(1);
-% subplot(1)
-% plot(0:maxorder,mean(foldLoss,1),'linewidth',2)
-% xlabel('Model Order');
-% ylabel('Loss');
-% title('CV Loss');
-%% use ^0 to set first data points to 1
-%fc = ones(size(xtrain)); 
-%fc_test = ones(size(xtest));
-%X = [fc];
-%Xtest = [fc_test];
-%%
-% Polynomial Order of n
-% Changing value of 'order' to choose polynomial order
-% order = 3;
-% X = [];
-% Xtest = [];
-
-% for k = 0:order
-%     X = [X xtrain.^k];
-%     Xtest = [Xtest xtest.^k];
-% end
-% 
-% % compute model parameters
-% w = inv(X'*X)*X'*ttrain; 
-% % model predictions
-% mpred_test = w'*Xtest';
-% 
-% compare = [ttest mpred_test' ttest-mpred_test'];
-% %
-% plot(xtrain,ttrain,'b.','markersize', 25); %plotting training data as scatterplot (blue)
-% hold on;
-% plot(xtrain, w'*X','b'); % plotting line of best fit (n order) to training data (blue)
-% plot(xtest,ttest,'ro'); % plotting test data as scatterplot (red circles)
-% plot(xtest,mpred_test,'r'); % plotting line of best fit (n order) to test data (red)
-% set(gca,'Color',[0.5 0.5 0.5]);
+%% Plot the results
+figure(1);
+subplot(121)
+plot(0:order,mean(foldLoss,1),'linewidth',2) % order vs mean of foldloss(vector)(column = order number?)
+xlabel('Model Order');
+ylabel('Loss');
+title('CV Loss');
+subplot(122)
+plot(0:order,mean(trainLoss,1),'linewidth',2)
+xlabel('Model Order');
+ylabel('Loss');
+title('Train Loss');
