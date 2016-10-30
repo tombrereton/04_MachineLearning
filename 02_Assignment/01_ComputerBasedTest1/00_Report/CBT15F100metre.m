0907c204377969b90ca59657e164da5520308576
tombrereton <tbererton89@gmail.com>
2016-10-30 02:05:40 +0000

    CBT1 report submitted

--- 02_Assignment/01_ComputerBasedTest1/00_Report/CBT15F100metre.m
+++ 02_Assignment/01_ComputerBasedTest1/00_Report/CBT15F100metre.m
@@ -17,6 +17,10 @@ Npartition = length(x(:,1)); % we find the length of the first column in matrix 
 partition = repmat(floor(Npartition/totalFolds),1,totalFolds); % we determine the size of the partition (2.7 in this case) and take the floor
 partition(end) = Npartition - (totalFolds-1)*partition(1,1);
 pcum = [0 cumsum(partition)];
+%% Permute data
+% order = randperm(Npartition);
+% x = x(order);
+% t = t(order);
 %% Polynomial Order of n
 % Change value of 'order' to choose polynomial order n
 order = 1; % order of polynomial
@@ -30,7 +34,7 @@ for k = 0:order % we loop to build the attribute set for order n
     X = [X x.^k]; % we add a column to matrix for every order n where the new column is x^k
 end
 
-for lambda = 0:0.1:1 % we loop through lambda values to vary the regularisation
+for lambda = 0:1:150 % we loop through lambda values to vary the regularisation
     for fold = 1:totalFolds % we loop again to change the partition being tested and trained
         Xfold = X(pcum(fold)+1 : pcum(fold+1),:); % we slice X to get the rows for the CV (test) partition (1)
         Xtrain1 = X(1 : pcum(fold),:); % we slice X to get the training partition before the CV partition (2)
@@ -61,17 +65,17 @@ for lambda = 0:0.1:1 % we loop through lambda values to vary the regularisation
     matCVLoss = [matCVLoss mean(foldLoss)];
     matLambda = [matLambda lambda];
     
-    % plots and saves to file
-    plot(x,t,'bx');
-    plotTitle = strcat('Fit of Polynomial Model x.^',int2str(k),' with lambda = ',num2str(lambda),' to Mens 100m Data'); 
-    title(plotTitle);
-    xlabel('Rescaled Year Data','fontsize',16);
-    ylabel('Time (seconds)','fontsize',16);
-    hold on;
-    plot(x,w'*X','r');
-    filename = strcat('LambdaModel',num2str(lambda),'_',int2str(order),'.png'); 
-    saveas(gcf,filename);
-    clf;
+%     % plots and saves to file
+%     plot(x,t,'bx');
+%     plotTitle = strcat('Fit of Polynomial Model x.^',int2str(k),' with lambda = ',num2str(lambda),' to Mens 100m Data'); 
+%     title(plotTitle);
+%     xlabel('Rescaled Year Data','fontsize',16);
+%     ylabel('Time (seconds)','fontsize',16);
+%     hold on;
+%     plot(x,w'*X','r');
+%     filename = strcat('LambdaModel',num2str(lambda),'_',int2str(order),'.png'); 
+%     saveas(gcf,filename);
+%     clf;
 end
 %% Plot the results
 % Reference: A First Course in Machine Learning, Chapter One, cv_demo.m
@@ -80,5 +84,5 @@ plot(matLambda,matCVLoss,'linewidth',2);
 xlabel('Lambda','fontsize',16);
 ylabel('Loss','fontsize',16);
 title('CV Loss vs Lambda for Olympic Mens 100m Data','fontsize',16);
-filename = strcat('CVLossvsLambda',int2str(order),'.png'); 
-saveas(gcf,filename);
+filename = strcat('CVLossvsLambdaRand',int2str(order),'.png'); 
+% saveas(gcf,filename);
