8afab9089c739a513cf8117e984b03c057dd03a1
tombrereton <tbererton89@gmail.com>
2016-11-05 11:45:56 +0000

    commenting bayesclass.m

--- 02_Assignment/02_ComputerBasedTest2/CBT2/bayesclass.m
+++ 02_Assignment/02_ComputerBasedTest2/CBT2/bayesclass.m
@@ -9,41 +9,41 @@ load bc_data
 
 % Plot the data
 
-cl = unique(t);
-col = {'ko','kd','ks'}
-fcol = {[1 0 0],[0 1 0],[0 0 1]};
+cl = unique(t); % get total number of classes from test set
+col = {'ko','kd','ks'}; 
+fcol = {[1 0 0],[0 1 0],[0 0 1]}; % colour of markers
 figure(1);
 hold off
 for c = 1:length(cl)
-    pos = find(t==cl(c));
-    plot(X(pos,1),X(pos,2),col{c},...
+    pos = find(t==cl(c)); % get position, or row, of each data point in class
+    plot(X(pos,1),X(pos,2),col{c},... % plot points of column 1 on x and column 2 on y
         'markersize',10,'linewidth',2,...
         'markerfacecolor',fcol{c});
     hold on
 end
-xlim([-3 7])
-ylim([-6 6])
+xlim([-3 7]) % set x scale on graph
+ylim([-6 6]) % set y scale on graph
 
 
 
 %% Fit class-conditional Gaussians for each class
 % Using the Naive (independence) assumption
 for c = 1:length(cl)
-    pos = find(t==cl(c));
+    pos = find(t==cl(c)); % we get the position, or row, of each point in a class
     % Find the means
-    class_mean(c,:) = mean(X(pos,:));
-    class_var(c,:) = var(X(pos,:),1);
+    class_mean(c,:) = mean(X(pos,:)); % we find the means for each class
+    class_var(c,:) = var(X(pos,:),1); % we find the variance for each class
 end
 
 
 %% Compute the predictive probabilities
-[Xv,Yv] = meshgrid(-3:0.1:7,-6:0.1:6);
+[Xv,Yv] = meshgrid(-3:0.1:7,-6:0.1:6); % what is this mesh grid for??, 
 Probs = [];
 for c = 1:length(cl)
-    temp = [Xv(:)-class_mean(c,1) Yv(:)-class_mean(c,2)];
-    tempc = diag(class_var(c,:));
-    const = -log(2*pi) - log(det(tempc));
-    Probs(:,:,c) = reshape(exp(const - 0.5*diag(temp*inv(tempc)*temp')),size(Xv));
+    temp = [Xv(:)-class_mean(c,1) Yv(:)-class_mean(c,2)]; % xnew - mean, tnew - mean
+    tempc = diag(class_var(c,:)); % only diagonal, assuming naive? , (co)variance for class
+    const = -log(2*pi) - log(det(tempc)); % is it actually easier with log?, why not -1/2 out front?
+    Probs(:,:,c) = reshape(exp(const - 0.5*diag(temp*inv(tempc)*temp')),size(Xv)); % reshape exp(of function) into size of Xv
 end
 
 Probs = Probs./repmat(sum(Probs,3),[1,1,3]);
---
+++ 04_Quiz/QuizHelper.m
@@ -0,0 +1,35 @@
+%% Clear
+clear all;close all;
+%% Variables
+x = [2.2, 3.3, 5, 4.8, 6.7, 8.9]';
+y = [1.1, 2.1, 2.3, 3.5, 4.9, 5.9]';
+% xnew = [1.1,2.1,3.1,4.0,4.9,5.9]';
+%% Model Order;
+order = 1;
+X = [];
+Xnew = [];
+for i = 0 : order;
+    X = [X x.^i];
+%     Xnew = [Xnew xnew.^i];
+end
+%% Learn model
+w = inv(X'*X)*X'*y;
+%% Compute Predictions
+% pred = w' * Xnew'; 
+pred = X * w;
+%% Loss
+difference = [x, pred, pred - x];
+loss = mean((pred - x).^2);
+%% Learn model numerically
+meanx = mean(x);
+meany = mean(y);
+meanxy = mean(x.*y);
+sqmeanx = mean(x)^2;
+meanxsq = mean(x.^2);
+w1 = (meanxy - meanx * meany) / (meanxsq - sqmeanx);
+w0 = meany - w1 * meanx;
+%% Plot model
+plot(x,y,'bx');
+hold on;
+plot(x, X * w, 'r');
+% plot(x, pred, 'b');
